1101ae35381352de5d79a973087264e3fe6c22fb
Doug Richar <drichar@gmail.com>
2023-10-04 01:32:37 -0400

    chore: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export * from './clients'
-export * from './constants'
+export { WalletManager } from './manager.ts'
+export { WALLET_ID } from './constants'
 export * from './types'
-export * from './utils'
-export * from './manager.ts'
-export * from './wallet.ts'
