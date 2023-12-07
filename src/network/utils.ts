a6feb2ec3e82edd062f287badaadfa9e88a93bbd
Doug Richar <drichar@gmail.com>
2023-12-07 00:09:34 -0500

    test(network): add unit tests for Network util functions

--- src/network/utils.ts
+++ src/network/utils.ts
@@ -5,6 +5,21 @@ export function isValidNetworkId(networkId: any): networkId is NetworkId {
   return Object.values(NetworkId).includes(networkId)
 }
 
+export function isAlgodConfig(config: any): config is AlgodConfig {
+  if (typeof config !== 'object') return false
+
+  for (const key of Object.keys(config)) {
+    if (!['token', 'baseServer', 'port', 'headers'].includes(key)) return false
+  }
+
+  return (
+    typeof config.token === 'string' &&
+    typeof config.baseServer === 'string' &&
+    ['string', 'number', 'undefined'].includes(typeof config.port) &&
+    ['object', 'undefined'].includes(typeof config.headers)
+  )
+}
+
 export function isNetworkConfigMap(config: NetworkConfig): config is NetworkConfigMap {
   const networkKeys = Object.values(NetworkId) as string[]
   return Object.keys(config).some((key) => networkKeys.includes(key))
