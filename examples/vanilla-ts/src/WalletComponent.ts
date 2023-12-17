b72a9956e28bab0be844033ef65617f04f693f67
Doug Richar <drichar@gmail.com>
2023-12-17 02:56:13 -0500

    feat: add Vite vanilla-ts example

---
+++ examples/vanilla-ts/src/WalletComponent.ts
@@ -0,0 +1,101 @@
+import { BaseWallet } from '@txnlab/use-wallet-js'
+
+export class WalletComponent {
+  wallet: BaseWallet
+  element: HTMLElement
+  private unsubscribe?: () => void
+
+  constructor(wallet: BaseWallet) {
+    this.wallet = wallet
+    this.element = document.createElement('div')
+
+    this.unsubscribe = wallet.subscribe((state) => {
+      console.log('State change:', state)
+      this.render()
+    })
+
+    this.render()
+    this.addEventListeners()
+  }
+
+  connect = () => this.wallet.connect()
+  disconnect = () => this.wallet.disconnect()
+  setActive = () => this.wallet.setActive()
+
+  setActiveAccount = (event: Event) => {
+    const target = event.target as HTMLSelectElement
+    this.wallet.setActiveAccount(target.value)
+  }
+
+  render() {
+    this.element.innerHTML = `
+      <h4>
+        ${this.wallet.metadata.name} ${this.wallet.isActive ? '[active]' : ''}
+      </h4>
+      <div>
+        <button id="connect-button" type="button" ${this.wallet.isConnected ? 'disabled' : ''}>
+            Connect
+        </button>
+        <button id="disconnect-button" type="button" ${!this.wallet.isConnected ? 'disabled' : ''}>
+            Disconnect
+        </button>
+        <button id="set-active-button" type="button" ${
+          !this.wallet.isConnected || this.wallet.isActive ? 'disabled' : ''
+        }>
+            Set Active
+        </button>
+      </div>
+      ${
+        this.wallet.isActive && this.wallet.accounts.length
+          ? `
+        <div>
+          <select>
+            ${this.wallet.accounts
+              .map(
+                (account) => `
+              <option value="${account.address}" ${
+                account.address === this.wallet.activeAccount?.address ? 'selected' : ''
+              }>
+                ${account.address}
+              </option>
+            `
+              )
+              .join('')}
+          </select>
+        </div>
+      `
+          : ''
+      }
+    `
+  }
+
+  addEventListeners() {
+    this.element.addEventListener('click', (e: Event) => {
+      const target = e.target as HTMLElement
+      if (target.id === 'connect-button') {
+        this.connect()
+      } else if (target.id === 'disconnect-button') {
+        this.disconnect()
+      } else if (target.id === 'set-active-button') {
+        this.setActive()
+      }
+    })
+
+    // Select a new active account
+    this.element.addEventListener('change', (e: Event) => {
+      const target = e.target as HTMLElement
+      if (target.tagName.toLowerCase() === 'select') {
+        this.setActiveAccount(e)
+      }
+    })
+  }
+
+  destroy() {
+    // Disconnect the listener on unmount to prevent memory leaks
+    if (this.unsubscribe) {
+      this.unsubscribe()
+    }
+    this.element.removeEventListener('click', this.addEventListeners)
+    this.element.removeEventListener('change', this.addEventListeners)
+  }
+}
---
+++ examples/vanilla-ts/src/main.ts
@@ -0,0 +1,52 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { NetworkId, WalletId, WalletManager } from '@txnlab/use-wallet-js'
+import { WalletComponent } from './WalletComponent'
+
+const walletManager = new WalletManager({
+  wallets: [
+    WalletId.DEFLY,
+    WalletId.EXODUS,
+    WalletId.PERA,
+    {
+      id: WalletId.WALLETCONNECT,
+      options: { projectId: 'fcfde0713d43baa0d23be0773c80a72b' }
+    },
+    WalletId.KMD
+  ],
+  network: NetworkId.TESTNET
+})
+
+const appDiv = document.querySelector<HTMLDivElement>('#app')
+
+appDiv!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>use-wallet-js (Vanilla TS)</h1>
+  </div>
+`
+
+const walletComponents = walletManager.wallets.map((wallet) => new WalletComponent(wallet))
+
+walletComponents.forEach((walletComponent) => {
+  appDiv?.appendChild(walletComponent.element)
+})
+
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    await walletManager.resumeSessions()
+  } catch (error) {
+    console.error('[App] Error resuming sessions:', error)
+  }
+})
+
+// Cleanup
+window.addEventListener('beforeunload', () => {
+  walletComponents.forEach((walletComponent) => walletComponent.destroy())
+})
