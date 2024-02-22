c2c8d3cd352c0cfdb65cf84c25735e35a77b816a
Doug Richar <drichar@gmail.com>
2024-02-22 01:23:42 -0500

    feat(nextjs): add Next.js example

---
+++ examples/nextjs/src/app/providers.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { NetworkId, WalletId, WalletManager } from '@txnlab/use-wallet-js'
+import { WalletProvider } from '@txnlab/use-wallet-react'
+
+const walletManager = new WalletManager({
+  wallets: [
+    WalletId.DEFLY,
+    WalletId.EXODUS,
+    WalletId.PERA,
+    {
+      id: WalletId.WALLETCONNECT,
+      options: { projectId: 'fcfde0713d43baa0d23be0773c80a72b' }
+    },
+    WalletId.KMD
+  ],
+  network: NetworkId.TESTNET
+})
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <WalletProvider manager={walletManager}>{children}</WalletProvider>
+}
